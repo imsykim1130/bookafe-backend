a55aa5de20cd2742bcea635850418d5c9eb7af8f
imsykim1130 <imsykim1130@gmail.com>
2024-11-10 16:20:42 +0900

    refactor: UserItem id 속성 추가
    프론트단에서 유저 식별을 자체적으로 하기 위한 id 값이 필요하여 응답 dto 에 id 속성 추가

--- src/main/java/study/back/dto/item/UserItem.java
+++ src/main/java/study/back/dto/item/UserItem.java
@@ -9,12 +9,14 @@ import study.back.entity.UserEntity;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserItem {
+    private Long id;
     private String email;
     private String nickname;
     private String profileImg;
 
     public static UserItem createUserItem(UserEntity user) {
         UserItem userItem = new UserItem();
+        userItem.id = user.getId();
         userItem.email = user.getEmail();
         userItem.nickname = user.getNickname();
         userItem.profileImg = user.getProfileImg();
