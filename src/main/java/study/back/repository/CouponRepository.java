b98c2e3bc1268fd61f91d5a32cc4714f989837f9
imsykim1130 <imsykim1130@gmail.com>
2024-11-22 15:39:32 +0900

    feat: 쿠폰 레포지토리 생성

---
+++ src/main/java/study/back/repository/CouponRepository.java
@@ -0,0 +1,8 @@
+package study.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.back.entity.CouponEntity;
+
+public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
+
+}
