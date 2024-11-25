11708cf6e88da4cd495297a195e5485456215f3d
imsykim1130 <imsykim1130@gmail.com>
2024-11-25 22:04:01 +0900

    feat: 유저의 보유 쿠폰 리스트 가져오기 api

--- src/main/java/study/back/controller/CouponController.java
+++ src/main/java/study/back/controller/CouponController.java
@@ -1,8 +1,13 @@
 package study.back.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.back.dto.response.GetUserCouponListResponseDto;
+import study.back.entity.UserEntity;
 import study.back.service.CouponService;
 
 @RestController
@@ -10,5 +15,10 @@ import study.back.service.CouponService;
 @RequestMapping("/api/v1/coupon")
 public class CouponController {
     private final CouponService couponService;
+
+    @GetMapping("")
+    public ResponseEntity<? super GetUserCouponListResponseDto> getUserCouponList(@AuthenticationPrincipal UserEntity user) {
+        return couponService.getCouponList(user);
+    }
 
 }
