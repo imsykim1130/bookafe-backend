e14dfe1f35e1374ce5701ab6062404c8cd036a4b
imsykim1130 <imsykim1130@gmail.com>
2024-11-25 22:06:41 +0900

    feat: 유저 쿠폰 리스트 받기 로직 구현

--- src/main/java/study/back/service/CouponService.java
+++ src/main/java/study/back/service/CouponService.java
@@ -1,10 +1,16 @@
 package study.back.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import study.back.dto.response.GetUserCouponListResponseDto;
+import study.back.dto.response.ResponseDto;
+import study.back.entity.CouponEntity;
+import study.back.entity.UserEntity;
 import study.back.repository.CouponRepository;
 import study.back.repository.UserRepository;
 
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +18,16 @@ public class CouponService {
     private final CouponRepository couponRepository;
     private final UserRepository userRepository;
 
+    public ResponseEntity<? super GetUserCouponListResponseDto> getCouponList(UserEntity user) {
+        List<CouponEntity> couponList;
+        try {
+            if(!userRepository.existsById(user.getId())) {
+                return ResponseDto.notFoundUser();
+            }
+            couponList = couponRepository.findAllByUser(user);
+        } catch (Exception e) {
+            return ResponseDto.internalServerError();
+        }
+        return GetUserCouponListResponseDto.success(couponList);
+    }
 }
