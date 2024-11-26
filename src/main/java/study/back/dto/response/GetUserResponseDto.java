b267a1e2c3290c0b87e02d54a10d7eaae1621252
imsykim1130 <imsykim1130@gmail.com>
2024-11-26 17:15:51 +0900

    feat: 유저 정보에 총 포인트 정보 추가

--- src/main/java/study/back/dto/response/GetUserResponseDto.java
+++ src/main/java/study/back/dto/response/GetUserResponseDto.java
@@ -12,11 +12,14 @@ import study.back.entity.UserEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetUserResponseDto extends ResponseDto {
     private UserItem user;
-    public static ResponseEntity<GetUserResponseDto> success(UserEntity user) {
+    private int totalPoint;
+
+    public static ResponseEntity<GetUserResponseDto> success(UserEntity user, int totalPoint) {
         GetUserResponseDto responseBody = new GetUserResponseDto();
         responseBody.code = "SU";
         responseBody.message = "유저 정보 받기 성공";
         responseBody.user = UserItem.createUserItem(user);
+        responseBody.totalPoint = totalPoint;
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 }
--- src/main/java/study/back/service/UserService.java
+++ src/main/java/study/back/service/UserService.java
@@ -6,14 +6,20 @@ import study.back.dto.response.GetUserOrderInfoResponseDto;
 import study.back.dto.response.GetUserResponseDto;
 import study.back.dto.response.ResponseDto;
 import study.back.entity.UserEntity;
+import study.back.repository.PointRepository;
 import study.back.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PointRepository pointRepository;
 
     public ResponseEntity<? super GetUserResponseDto> getUser(UserEntity user) {
+        int totalPoint;
+
         try {
             // 토큰에서 얻은 정보로 db 에 해당 유저 있는지 찾기
             // 인증 시 db 에서 유저 정보를 가져와서 토큰과 비교하기 때문에 이미 db 검증이 끝났다고 보고
@@ -21,10 +27,13 @@ public class UserService {
             if(!userRepository.existsByEmail(user.getEmail())) {
                 return ResponseDto.notFoundUser();
             }
+            Optional<Integer> totalPointOpt = pointRepository.getTotalPointByUser(user);
+            totalPoint = totalPointOpt.orElse(0);
+
         } catch (Exception e) {
             return ResponseDto.internalServerError();
         }
-        return GetUserResponseDto.success(user);
+        return GetUserResponseDto.success(user, totalPoint);
     }
 
     public ResponseEntity<? super GetUserOrderInfoResponseDto> getUserOrderInfo(UserEntity user) {
