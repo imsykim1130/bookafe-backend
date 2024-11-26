ec8e90c74e7c4b844aeed464e027028d50654092
imsykim1130 <imsykim1130@gmail.com>
2024-11-26 17:20:03 +0900

    feat: 유저의 포인트 얻기 쿼리

--- src/main/java/study/back/repository/PointRepository.java
+++ src/main/java/study/back/repository/PointRepository.java
@@ -1,8 +1,14 @@
 package study.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.back.entity.PointEntity;
+import study.back.entity.UserEntity;
+
+import java.util.Optional;
 
 public interface PointRepository extends JpaRepository<PointEntity, Long> {
+    @Query("select sum(p.changedPoint) from PointEntity p where p.user = ?1")
+    Optional<Integer> getTotalPointByUser(UserEntity user);
 
 }
