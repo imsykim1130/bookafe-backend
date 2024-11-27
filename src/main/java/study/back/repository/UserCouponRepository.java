b06939ac89eb62c9aa35d36987a07c0ae3652f47
imsykim1130 <imsykim1130@gmail.com>
2024-11-27 22:39:41 +0900

    feat: 유저 보유쿠폰 불러오기 쿼리

--- src/main/java/study/back/repository/UserCouponRepository.java
+++ src/main/java/study/back/repository/UserCouponRepository.java
@@ -1,7 +1,16 @@
 package study.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.back.entity.UserCouponEntity;
+import study.back.entity.UserEntity;
+import study.back.repository.resultSet.UserCouponView;
+
+import java.util.List;
 
 public interface UserCouponRepository extends JpaRepository<UserCouponEntity, Long> {
+    // 유저의 보유쿠폰 불러오기
+    @Query("select us.id as id, us.coupon.name as name, us.coupon.discountPercent as discountPercent from UserCouponEntity us where us.user = ?1 and us.pending is false")
+    List<UserCouponView> findAllByUser(UserEntity user);
+
 }
