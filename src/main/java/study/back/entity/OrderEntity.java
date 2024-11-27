0a70c7818dec9a965e146793343133037bbf9de7
imsykim1130 <imsykim1130@gmail.com>
2024-11-28 00:16:07 +0900

    feat: OrderEntity 생성자 builder

--- src/main/java/study/back/entity/OrderEntity.java
+++ src/main/java/study/back/entity/OrderEntity.java
@@ -2,8 +2,10 @@ package study.back.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import study.back.utils.CustomUtil;
 
 @Entity
 @Getter
@@ -19,7 +21,20 @@ public class OrderEntity {
     private String orderDatetime;
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    @Builder
+    public static OrderEntity createEntity(String address, String addressDetail, String phoneNumber, UserEntity user) {
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.address = address;
+        orderEntity.addressDetail = addressDetail;
+        orderEntity.phoneNumber = phoneNumber;
+        orderEntity.orderDatetime = CustomUtil.getDateTime();
+        orderEntity.orderStatus = OrderStatus.READY;
+        orderEntity.user = user;
+        return orderEntity;
+    }
 }
