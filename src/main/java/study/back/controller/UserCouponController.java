d96f5af89e8d40a29c2e384e726bfd388aeb7cd9
imsykim1130 <imsykim1130@gmail.com>
2024-11-28 00:04:29 +0900

    feat: 사용한 쿠폰 삭제 api

--- src/main/java/study/back/controller/UserCouponController.java
+++ src/main/java/study/back/controller/UserCouponController.java
@@ -15,13 +15,22 @@ import study.back.service.UserCouponService;
 public class UserCouponController {
     private final UserCouponService userCouponService;
 
+    // 유저 보유 쿠폰 리스트 받기
     @GetMapping("")
     public ResponseEntity<? super GetUserCouponListResponseDto> getUserCouponList(@AuthenticationPrincipal UserEntity user) {
         return userCouponService.getCouponList(user);
     }
+
+    // 쿠폰 pending 상태 변경
     @PutMapping("/pending/{userCouponId}")
     public ResponseEntity<ResponseDto> updateUserCouponPending(@PathVariable(name="userCouponId") Long userCouponId) {
         return userCouponService.updateUserCouponPending(userCouponId);
+    }
+
+    // 사용한 쿠폰 삭제
+    @DeleteMapping("/{userCouponId}")
+    public ResponseEntity<ResponseDto> deleteUserCoupon(@PathVariable(name="userCouponId") Long userCouponId) {
+        return userCouponService.deleteUserCoupon(userCouponId);
     }
 
 }
