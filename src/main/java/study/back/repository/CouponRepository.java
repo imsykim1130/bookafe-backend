9b604ef177bba69bf3e830503358f4bec408ae4c
imsykim1130 <imsykim1130@gmail.com>
2024-11-27 22:39:11 +0900

    refactor: 레포지토리에 맞지 않는 쿼리 삭제

--- src/main/java/study/back/repository/CouponRepository.java
+++ src/main/java/study/back/repository/CouponRepository.java
@@ -1,15 +1,9 @@
 package study.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import study.back.entity.CouponEntity;
-import study.back.entity.UserEntity;
 
-import java.util.List;
 
 public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
-    // 유저의 보유쿠폰 불러오기
-    @Query("select us.coupon from UserCouponEntity us where us.user = ?1 and us.coupon.pending is false")
-    List<CouponEntity> findAllByUser(UserEntity user);
 
 }
