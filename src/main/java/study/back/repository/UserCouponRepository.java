9fda417257def12498bc9ee6bed2ee296708f32b
imsykim1130 <imsykim1130@gmail.com>
2024-11-28 16:50:30 +0900

    feat: 쿠폰 pending 상태 변경 쿼리

--- src/main/java/study/back/repository/UserCouponRepository.java
+++ src/main/java/study/back/repository/UserCouponRepository.java
@@ -1,7 +1,9 @@
 package study.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.back.entity.UserCouponEntity;
 import study.back.entity.UserEntity;
 import study.back.repository.resultSet.UserCouponView;
@@ -12,5 +14,9 @@ public interface UserCouponRepository extends JpaRepository<UserCouponEntity, Lo
     // 유저의 보유쿠폰 불러오기
     @Query("select us.id as id, us.coupon.name as name, us.coupon.discountPercent as discountPercent from UserCouponEntity us where us.user = ?1 and us.pending is false")
     List<UserCouponView> findAllByUser(UserEntity user);
+
+    @Modifying
+    @Query("update UserCouponEntity uc set uc.pending = :pending where uc.id in :userCouponIdList")
+    int updateUserCouponPending(@Param("pending") String pending, @Param("userCouponIdList") List<Long> userCouponIdList);
 
 }
