4d57258d90dd796c59abcdde634703b3ae2c47c7
imsykim1130 <imsykim1130@gmail.com>
2024-11-28 19:32:46 +0900

    feat: OrderBookRepository 생성

---
+++ src/main/java/study/back/repository/OrderBookRepository.java
@@ -0,0 +1,8 @@
+package study.back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.back.entity.OrderBookEntity;
+
+public interface OrderBookRepository extends JpaRepository<OrderBookEntity, Long> {
+
+}
