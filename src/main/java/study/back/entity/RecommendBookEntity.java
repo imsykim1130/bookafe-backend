733acb17132af24fdac5090f1dbab0a508029248
imsykim1130 <imsykim1130@gmail.com>
2025-01-10 17:31:38 +0900

    feat: RecommendBookEntity 생성자 추가

--- src/main/java/study/back/entity/RecommendBookEntity.java
+++ src/main/java/study/back/entity/RecommendBookEntity.java
@@ -1,13 +1,13 @@
 package study.back.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 @Table(name="recommend_book")
 public class RecommendBookEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
