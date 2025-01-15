43f99dbbd2773e5a3a1562f85d59e5f5ff38ccf8
imsykim1130 <imsykim1130@gmail.com>
2025-01-15 20:28:23 +0900

    chore: 사용 레포지토리 변수명 수정

--- src/main/java/study/back/service/implement/BookCartServiceImpl.java
+++ src/main/java/study/back/service/implement/BookCartServiceImpl.java
@@ -17,49 +17,49 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class BookCartServiceImpl implements BookCartService {
-    private final BookCartRepositoryInterface bookCartRepository;
+    private final BookCartRepositoryInterface repository;
 
     // 장바구니 담기
     public void putBookToCart(UserEntity user, String isbn) {
         System.out.println("장바구니 담기");
         // 책 여부 검증
-        bookCartRepository.getBookByIsbn(isbn).orElseThrow(() -> new NotExistBookException("책이 존재하지 않습니다"));
+        repository.getBookByIsbn(isbn).orElseThrow(() -> new NotExistBookException("책이 존재하지 않습니다"));
 
         // 장바구니 여부 검증
-        boolean isAlreadyCartBook = bookCartRepository.existsByIsbn(isbn);
+        boolean isAlreadyCartBook = repository.existsByIsbn(isbn);
         if (isAlreadyCartBook) {
             throw new RuntimeException("이미 장바구니에 존재하는 책입니다");
         }
         BookCartEntity bookCart = BookCartEntity.createBookCart(user, isbn);
-        bookCartRepository.save(bookCart);
+        repository.save(bookCart);
     }
 
     // 장바구니 빼기
     public void deleteBookFromCart(String isbn) {
         System.out.println("장바구니 빼기");
-        BookCartEntity bookCart = bookCartRepository.findByIsbn(isbn).orElseThrow(() -> new RuntimeException("이미 장바구니에 없는 책입니다"));
-        bookCartRepository.delete(bookCart);
+        BookCartEntity bookCart = repository.findByIsbn(isbn).orElseThrow(() -> new RuntimeException("이미 장바구니에 없는 책입니다"));
+        repository.delete(bookCart);
     }
 
     // 장바구니 여부
     @Override
     public boolean isCart(String isbn) {
         System.out.println("장바구니 여부");
-        return bookCartRepository.existsByIsbn(isbn);
+        return repository.existsByIsbn(isbn);
     }
 
     // 장바구니 책 리스트 가져오기
     @Override
     public List<CartBookView> getBookCartList(UserEntity user) {
         System.out.println("장바구니 책 리스트 가져오기");
-        return bookCartRepository.findCartBookViewListByUser(user);
+        return repository.findCartBookViewListByUser(user);
     }
 
     // 장바구니 책 여러개 동시 삭제
     @Override
     public void deleteList(List<Long> cartBookIdList) {
         System.out.println("장바구니 책 여러개 동시 삭제");
-        bookCartRepository.deleteAllByIdList(cartBookIdList);
+        repository.deleteAllByIdList(cartBookIdList);
     }
 
     // 장바구니 책 수량 변경
@@ -69,8 +69,7 @@ public class BookCartServiceImpl implements BookCartService {
         if(count <= 0) {
             throw new RuntimeException("잘못된 수량입니다");
         }
-        BookCartEntity bookCart = bookCartRepository.findByIsbnAndUser(isbn, user).orElseThrow(() -> new RuntimeException("장바구니에 존재하지 않습니다"));
+        BookCartEntity bookCart = repository.findByIsbnAndUser(isbn, user).orElseThrow(() -> new RuntimeException("장바구니에 존재하지 않습니다"));
         bookCart.changeCount(count);
-//        bookCartRepository.save(bookCart);
     }
 }
