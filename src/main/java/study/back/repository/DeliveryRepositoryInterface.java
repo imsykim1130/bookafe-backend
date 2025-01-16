eb464e7b6e19ea9d371a9bb5125fe51788015bb0
imsykim1130 <imsykim1130@gmail.com>
2025-01-17 05:43:56 +0900

    chore: 미사용 삭제

--- src/main/java/study/back/repository/DeliveryRepositoryInterface.java
+++ src/main/java/study/back/repository/DeliveryRepositoryInterface.java
@@ -1,11 +1,9 @@
 package study.back.repository;
 
 import study.back.entity.OrderEntity;
-import study.back.entity.OrderStatus;
 
 import java.util.Optional;
 
 public interface DeliveryRepositoryInterface {
-    void updateOrderStatus(Long orderId, OrderStatus orderStatus);
     Optional<OrderEntity> findOrderByOrderId(Long orderId);
 }
