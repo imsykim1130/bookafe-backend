f6820f9459311f3cb4950cf4ce0b484e5984829c
imsykim1130 <imsykim1130@gmail.com>
2025-01-17 05:59:41 +0900

    chore: 미사용 삭제

--- src/main/java/study/back/repository/impl/DeliveryRepoImpl.java
+++ src/main/java/study/back/repository/impl/DeliveryRepoImpl.java
@@ -1,10 +1,8 @@
 package study.back.repository.impl;
 
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import study.back.entity.OrderEntity;
-import study.back.entity.OrderStatus;
 import study.back.repository.DeliveryRepositoryInterface;
 import study.back.repository.origin.OrderRepository;
 
@@ -13,16 +11,7 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class DeliveryRepoImpl implements DeliveryRepositoryInterface {
-    private final EntityManager em;
     private final OrderRepository orderRepository;
-
-    @Override
-    public void updateOrderStatus(Long orderId, OrderStatus orderStatus) {
-        em.createQuery("update OrderEntity o set o.orderStatus = :orderStatus where o.id = :orderId")
-                .setParameter("orderId", orderId)
-                .setParameter("orderStatus", orderStatus)
-                .executeUpdate();
-    }
 
     @Override
     public Optional<OrderEntity> findOrderByOrderId(Long orderId) {
