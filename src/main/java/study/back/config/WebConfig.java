020c1f301730a4f944bdf70d8dfa687bb0e2d7ae
imsykim1130 <imsykim1130@gmail.com>
2025-01-24 15:39:53 +0900

    fix: WebConfig 의 이미지 리소스 주소 변경

--- src/main/java/study/back/config/WebConfig.java
+++ src/main/java/study/back/config/WebConfig.java
@@ -1,15 +1,9 @@
 package study.back.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.Collections;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -26,6 +20,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/image/**") // 해당 주소로 요청이 들어오면 아래 경로에서 이미지를 찾는다.
-                .addResourceLocations("file:/Users/gimsoyeong/Desktop/all/toy-project/bookafe/back/src/main/resources/static/image/");
+                .addResourceLocations("file:/Users/gimsoyeong/Desktop/all/toy-project/bookafe/bookafe-back/src/main/resources/static/image/");
     }
 }
