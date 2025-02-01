44d93572f65c87e2676105fbffa40e868040700f
imsykim1130 <imsykim1130@gmail.com>
2025-02-02 04:40:46 +0900

    refact: 에러 응답을 ResponseDto 객체로 변경하여 JSON 형식으로 반환하도록 수정

--- src/main/java/study/back/security/JwtFilter.java
+++ src/main/java/study/back/security/JwtFilter.java
@@ -1,4 +1,5 @@
 package study.back.security;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import study.back.dto.response.ResponseDto;
 
 import java.io.IOException;
 
@@ -19,6 +21,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtils jwtUtils;
     private final UserDetailsServiceImpl userDetailsService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -33,23 +36,25 @@ public class JwtFilter extends OncePerRequestFilter {
             }
 
             // 토큰 확인 후 인증 정보 저장하기
+            // 시큐리티는 SecurityContextHolder 에 인증 정보가 들어 있을 때 인증 성공으로 간주하기 때문에 헤더에서 받은 토큰이 검증된 토큰이면 context 에 인증 정보를 넣어준다.
             if(jwtUtils.validateToken(jwt)) {
                 String email = jwtUtils.getEmailFromToken(jwt);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(email);
                 Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
-
+         // 토큰 validate 중 에러 발생
         } catch (JwtException e) {
-            // 토큰 validate 중 에러 발생
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("올바르지 않은 토큰 또는 만료된 토큰입니다.");
+            ResponseDto responseDto = ResponseDto.builder().code("UA").message("올바르지 않은 토큰 또는 만료된 토큰입니다").build();
+            response.getWriter().write(objectMapper.writeValueAsString(responseDto));
             return;
 
+          // 서버 에러
         } catch(Exception e) {
-            // 서버 에러
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            response.getWriter().write("서버 오류");
+            ResponseDto responseDto = ResponseDto.builder().code("ISE").message("서버 에러").build();
+            response.getWriter().write(objectMapper.writeValueAsString(responseDto));
             return;
         }
 
