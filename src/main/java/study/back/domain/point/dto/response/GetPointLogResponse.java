a8b04ee2dd6927a2eaee7d747a02f8e7cb2793f8
imsykim1130 <imsykim1130@gmail.com>
2025-02-08 19:38:16 +0900

    Refactor: ResponseDto 상속하여 응답에 code, message 항목 추가

--- src/main/java/study/back/domain/point/dto/response/GetPointLogResponse.java
+++ src/main/java/study/back/domain/point/dto/response/GetPointLogResponse.java
@@ -1,17 +1,27 @@
 package study.back.domain.point.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import study.back.utils.ResponseDto;
 import study.back.utils.item.PointLogView;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-public class GetPointLogResponse {
+public class GetPointLogResponse extends ResponseDto {
     private boolean isFirst;
     private boolean isLast;
     private List<PointLogView> pointLogList;
+
+    public GetPointLogResponse(boolean isFirst, boolean isLast, List<PointLogView> pointLogList) {
+        this("SU", "포인트 히스토리 가져오기 성공", isFirst, isLast, pointLogList);
+    }
+
+    public GetPointLogResponse(String code, String message, boolean isFirst, boolean isLast, List<PointLogView> pointLogList) {
+        super(code, message);
+        this.isFirst = isFirst;
+        this.isLast = isLast;
+        this.pointLogList = pointLogList;
+    }
 }
