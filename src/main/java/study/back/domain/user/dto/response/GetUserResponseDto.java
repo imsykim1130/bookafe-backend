5293290421718503b6f7bc8d99f26ed1f8ba2c9d
imsykim1130 <imsykim1130@gmail.com>
2025-02-28 21:57:21 +0900

    fix: 유저 정보 가져오기 응답 반환 형태 변경

--- src/main/java/study/back/domain/user/dto/response/GetUserResponseDto.java
+++ src/main/java/study/back/domain/user/dto/response/GetUserResponseDto.java
@@ -5,19 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import study.back.domain.user.entity.RoleName;
 import study.back.utils.ResponseDto;
-import study.back.utils.item.UserItem;
 import study.back.domain.user.entity.UserEntity;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetUserResponseDto extends ResponseDto {
-    private UserItem user;
+    private Long id;
+    private String email;
+    private String nickname;
+    private String profileImg;
+    private LocalDateTime createDate;
+    private RoleName role;
     private Long totalPoint;
 
-    public GetUserResponseDto(String code, String message, UserItem user, Long totalPoint) {
+    public GetUserResponseDto(String code, String message, UserEntity user, Long totalPoint) {
         super(code, message);
-        this.user = user;
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+        this.profileImg = user.getProfileImg();
+        this.createDate = user.getCreateDate();
+        this.role = user.getRole();
         this.totalPoint = totalPoint;
     }
 
@@ -25,7 +37,7 @@ public class GetUserResponseDto extends ResponseDto {
         GetUserResponseDto responseBody = new GetUserResponseDto(
                 "SU",
                 "유저 정보 받기 성공",
-                UserItem.createUserItem(user),
+                user,
                 totalPoint
         );
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
