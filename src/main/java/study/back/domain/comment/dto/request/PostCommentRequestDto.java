5d55a3dfc2e31fbb474e20cf642465de676eb9d0
imsykim1130 <imsykim1130@gmail.com>
2025-03-02 15:27:39 +0900

    fix: dto 기본 생성자 추가

--- src/main/java/study/back/domain/comment/dto/request/PostCommentRequestDto.java
+++ src/main/java/study/back/domain/comment/dto/request/PostCommentRequestDto.java
@@ -3,10 +3,12 @@ package study.back.domain.comment.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class PostCommentRequestDto {
     private Long parentId;
     private String isbn;
