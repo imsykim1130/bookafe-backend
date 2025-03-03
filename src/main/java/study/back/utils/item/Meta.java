9d0ca45309b466c9e00a95914e265a14a1141a11
imsykim1130 <imsykim1130@gmail.com>
2025-03-04 07:13:07 +0900

    fix: dto 기본 생성자 추가

--- src/main/java/study/back/utils/item/Meta.java
+++ src/main/java/study/back/utils/item/Meta.java
@@ -2,10 +2,12 @@ package study.back.utils.item;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Meta {
     private Boolean is_end;
     private Integer pageable_count;
--- src/main/java/study/back/utils/item/OriginBookItem.java
+++ src/main/java/study/back/utils/item/OriginBookItem.java
@@ -2,12 +2,14 @@ package study.back.utils.item;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class OriginBookItem {
     private Meta meta;
     private List<BookItem> documents;
