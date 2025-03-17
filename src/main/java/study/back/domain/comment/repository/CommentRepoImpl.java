388151091b9838a78d039304721ef089e5b25590
imsykim1130 <imsykim1130@gmail.com>
2025-03-17 18:27:32 +0900

    feat: CommentItem 에 userId 필드 추가
    - 리뷰를 작성한 유저의 페이지로 이동하기 위한 식별자로
      userId 사용하기 위함
    - CommentRepoImpl.java 파일에서 userId를 선택하도록 쿼리 수정

--- src/main/java/study/back/domain/comment/repository/CommentRepoImpl.java
+++ src/main/java/study/back/domain/comment/repository/CommentRepoImpl.java
@@ -49,7 +49,7 @@ public class CommentRepoImpl implements CommentRepository {
 
     @Override
     public List<CommentItem> findAllReplyByParentCommentId(Long parentCommentId) {
-        return em.createQuery("select c.id as id, u.profileImg as profileImg, u.nickname as nickname, c.writeDate as writeDate, c.emoji as emoji, c.content as content, (select count(c2) from CommentEntity c2 where c2.parent = c) as replyCount, c.isDeleted as isDeleted from CommentEntity c join UserEntity u on u.id = c.userId where c.parent.id = :parentCommentId order by c.writeDate", CommentItem.class)
+        return em.createQuery("select c.id as id, u.id as userId, u.profileImg as profileImg, u.nickname as nickname, c.writeDate as writeDate, c.emoji as emoji, c.content as content, (select count(c2) from CommentEntity c2 where c2.parent = c) as replyCount, c.isDeleted as isDeleted from CommentEntity c join UserEntity u on u.id = c.userId where c.parent.id = :parentCommentId order by c.writeDate", CommentItem.class)
                 .setParameter("parentCommentId", parentCommentId)
                 .getResultList();
     }
