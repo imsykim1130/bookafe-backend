aaf7fd206f4fe5be620cd04f80ce009d540ef240
imsykim1130 <imsykim1130@gmail.com>
2025-03-24 01:02:25 +0900

    fix: UserController 에 검증 추가 및 경로 수정
    - CloudinaryErrorException 추가
    - @NotEmpty 및 @NotNull 검증 어노테이션 추가
    - @RequestMapping 경로 변경 및 @GetMapping 등 세부 경로 수정

--- src/main/java/study/back/domain/user/controller/UserController.java
+++ src/main/java/study/back/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package study.back.domain.user.controller;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,16 +20,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
 
-    // 로그인 유저 정보 가져오기
+    // 유저 정보 가져오기
     /**
      * @param user
      * @return id, email, nickname, profileImg, createDate, role, totalPoint
      */
-    @GetMapping("")
+    @GetMapping("/user")
     public ResponseEntity<GetUserResponseDto> getUser(
             @AuthenticationPrincipal UserEntity user,
             @RequestParam(name = "userId", required = false) Long userId) {
@@ -40,53 +42,53 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(data);
     }
 
-    // 유저 검색
     /**
+     * 유저 검색
      * @param searchWord : 유저 이메일에 포함된 단어
      * @return
      */
-    @GetMapping("/search")
-    public ResponseEntity<List<UserManagementInfo>> getSearchUserList(@RequestParam(name = "searchWord") String searchWord,
+    @GetMapping("/admin/user/search")
+    public ResponseEntity<List<UserManagementInfo>> getSearchUserList(@NotEmpty(message = "검색 단어가 올바르지 않습니다") @RequestParam(name = "searchWord") String searchWord,
                                                                       @RequestParam(name="filter", required = false, defaultValue = "email") String filter) {
         List<UserManagementInfo> result = userService.getSearchUserList(searchWord);
         return ResponseEntity.ok().body(result);
     }
 
-    // 프로필 이미지 변경하기
-    // 클라우드 상에서 같은 유저는 같은 파일 이름으로 저장되기 때문에
-    // 기존에 이미지가 없다가 새로 추가하는 것이 아니라면 한 번 받은 이미지 url 계속 사용 가능
     /**
+     * 프로필 이미지 변경하기<br/>
+     * 클라우드 상에서 같은 유저는 같은 파일 이름으로 저장되기 때문에
+     * 기존에 이미지가 없다가 새로 추가하는 것이 아니라면 한 번 받은 이미지 url 계속 사용 가능
      * @param user
      * @param file
      * @return image url
      */
-    @PostMapping(value = "/profile-image", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<String> changeProfileImage(@AuthenticationPrincipal UserEntity user, @RequestPart(name = "file") MultipartFile file) {
+    @PostMapping(value = "/user/profile-image", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<String> changeProfileImage(@AuthenticationPrincipal UserEntity user, @NotNull(message = "파일이 존재하지 않습니다") @RequestPart(name = "file") MultipartFile file) {
         String result = userService.changeProfileImage(user, file);
         return ResponseEntity.ok(result);
     }
 
-    // 프로필 이미지 초기화
     /**
+     * 프로필 이미지 초기화
      * @param user: 로그인 된 유저
      * @return
      */
-    @DeleteMapping("/profile-image")
+    @DeleteMapping("/user/profile-image")
     public ResponseEntity<ResponseDto> deleteProfileImage(@AuthenticationPrincipal UserEntity user) {
         userService.initProfileImage(user);
         return ResponseEntity.ok(new ResponseDto("SU", "프로필 이미지 초기화 성공"));
     }
 
     // 탈퇴하기
-    @DeleteMapping("")
+    @DeleteMapping("/user")
     public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal UserEntity user) {
         userService.deleteUser(user);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     // 유저 탈퇴시키기
-    @DeleteMapping("/admin")
-    public ResponseEntity<Void> deleteUserByAdmin(@RequestParam("userId") Long userId) {
+    @DeleteMapping("/admin/user")
+    public ResponseEntity<Void> deleteUserByAdmin(@Min(value = 0, message = "올바르지 않은 user id 입니다") @RequestParam("userId") Long userId) {
         userService.deleteUserByAdmin(userId);
         return ResponseEntity.ok().build();
     }
