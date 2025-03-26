680f688513a3b2f50a53c6ed2238fd4a3e4c8252
imsykim1130 <imsykim1130@gmail.com>
2025-03-26 18:13:51 +0900

    refactor: UserEntity 수정자 접근 수준 변경
    - lombok @NoArgsConstructor의 접근 수준을 PROTECTED로 수정

--- src/main/java/study/back/domain/user/entity/UserEntity.java
+++ src/main/java/study/back/domain/user/entity/UserEntity.java
@@ -1,10 +1,7 @@
 package study.back.domain.user.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,7 +14,7 @@ import java.util.*;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class UserEntity implements UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
