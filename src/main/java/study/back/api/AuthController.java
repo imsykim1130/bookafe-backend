9c58dcbd764f270365e46f206d152fe13167a7f9
imsykim1130 <imsykim1130@gmail.com>
2025-04-09 17:49:31 +0900

    docs: 회원가입 메서드에 주석 추가
    - signUp 메서드에 대한 설명 및 파라미터 주석 추가

--- src/main/java/study/back/api/AuthController.java
+++ src/main/java/study/back/api/AuthController.java
@@ -1,34 +1,44 @@
 package study.back.api;
 
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import study.back.domain.user.service.AuthService;
 import study.back.global.dto.request.AuthWithGoogleRequestDto;
 import study.back.global.dto.request.SignInRequestDto;
 import study.back.global.dto.request.SignUpRequestDto;
 import study.back.global.dto.response.GetUserResponseDto;
-import study.back.domain.user.service.AuthService;
-import study.back.domain.user.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final AuthService authService;
-    private final UserService userService;
 
-    // 회원가입
+    /**
+     * 회원가입
+     * @param requestDto
+     * @return
+     */
     @PostMapping("/sign-up")
     public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequestDto requestDto) {
         authService.signUp(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    // 로그인
+    /**
+     * 로그인
+     * @param requestDto
+     * @return
+     */
     @PostMapping("/sign-in")
     public ResponseEntity<GetUserResponseDto> signIn(@Valid @RequestBody SignInRequestDto requestDto) {
         return authService.signIn(requestDto);
@@ -45,7 +55,10 @@ public class AuthController {
         return authService.authWithGoogle(requestDto);
     }
 
-    // 로그아웃
+    /**
+     * 로그아웃
+     * @return
+     */
     @PostMapping("/sign-out")
     public ResponseEntity<Void> logout() {
         ResponseCookie cookie = ResponseCookie.from("jwt", null)
