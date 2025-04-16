cb45774bad992d35dddee3d2a8c1f70672eeb3f0
imsykim1130 <imsykim1130@gmail.com>
2025-04-16 19:24:03 +0900

    fix: 회원 가입 요청 DTO 닉네임 유효성 검사 수정
    - 정규식을 ^[가-힣a-zA-Z0-9]{5,15}$로 변경하여 닉네임의 형식을 수정

--- src/main/java/study/back/global/dto/request/SignUpRequestDto.java
+++ src/main/java/study/back/global/dto/request/SignUpRequestDto.java
@@ -11,7 +11,7 @@ public class SignUpRequestDto {
     private String email;
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,15}$", message = "비밀번호 형태가 올바르지 않습니다")
     private String password;
-    @Pattern(regexp = "^(?=.*[A-Za-z])[A-Za-z\\d]{5,15}$", message = "닉네임 형태가 올바르지 않습니다")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{5,15}$", message = "닉네임 형태가 올바르지 않습니다")
     private String nickname;
     @Pattern(regexp = "^(user|admin)$", message = "역할은 user 와 admin 만 가능합니다")
     private String role;
