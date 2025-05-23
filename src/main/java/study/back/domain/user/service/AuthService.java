26c7ba39614a7d15edeb8a6f1424adcadafbfa1b
imsykim1130 <imsykim1130@gmail.com>
2025-05-23 16:30:01 +0900

    refactor: Firebase 인증 삭제 및 AuthRepository 로 대체
    - Firebase 에서 OAuth2.0으로 인증 방식이 변경됨에 따라 Firebase 관련
    코드를 삭제하고 AuthRepository 인터페이스로 대체
    
    - AuthServiceImpl 에서 Firebase 관련 의존성을 제거하고, Google 인증
    관련 메서드 및 DTO 를 삭제

--- src/main/java/study/back/domain/user/service/AuthService.java
+++ src/main/java/study/back/domain/user/service/AuthService.java
@@ -1,7 +1,6 @@
 package study.back.domain.user.service;
 
 import org.springframework.http.ResponseEntity;
-import study.back.global.dto.request.AuthWithGoogleRequestDto;
 import study.back.global.dto.request.SignInRequestDto;
 import study.back.global.dto.request.SignUpRequestDto;
 import study.back.global.dto.response.GetUserResponseDto;
@@ -10,5 +9,4 @@ import study.back.global.dto.response.SignUpResponseDto;
 public interface AuthService {
     ResponseEntity<GetUserResponseDto> signIn(SignInRequestDto signInRequestDto);
     SignUpResponseDto signUp(SignUpRequestDto signUpRequestDto);
-    ResponseEntity<GetUserResponseDto> authWithGoogle(AuthWithGoogleRequestDto requestDto);
 }
--- src/main/java/study/back/domain/user/service/AuthServiceImpl.java
+++ src/main/java/study/back/domain/user/service/AuthServiceImpl.java
@@ -4,18 +4,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
 
 import lombok.RequiredArgsConstructor;
 import study.back.domain.user.entity.RoleName;
 import study.back.domain.user.entity.UserEntity;
-import study.back.domain.user.repository.jpa.UserJpaRepository;
+import study.back.domain.user.repository.AuthRepository;
 import study.back.global.dto.request.AuthWithGoogleRequestDto;
 import study.back.global.dto.request.SignInRequestDto;
 import study.back.global.dto.request.SignUpRequestDto;
@@ -31,10 +26,9 @@ import study.back.global.security.JwtUtils;
 @RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService {
-    private final UserJpaRepository userJpaRepository;
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
-    private final FirebaseAuth firebaseAuth;
 
     // 로그인
     @Override
@@ -43,7 +37,7 @@ public class AuthServiceImpl implements AuthService {
         String password = signInRequestDto.getPassword();
 
         // 유저 가입 유무 확인
-        UserEntity savedUser = userJpaRepository.findByEmail(email);
+        UserEntity savedUser = authRepository.findUserByEmail(email);
 
         if(savedUser == null) {
             throw new UserNotFoundException();
@@ -56,16 +50,9 @@ public class AuthServiceImpl implements AuthService {
 
 
         // jwt 쿠키 반환
-        ResponseCookie cookie = createCookie(savedUser, "/", true, "None", 60 * 60);
+        ResponseCookie cookie = jwtUtils.createCookie(savedUser, "/", true, "None", 60 * 60);
 
-        GetUserResponseDto responseBody = GetUserResponseDto.builder()
-                .id(savedUser.getId())
-                .email(savedUser.getEmail())
-                .nickname(savedUser.getNickname())
-                .role(savedUser.getRole())
-                .profileImg(savedUser.getProfileImg())
-                .createDate(savedUser.getCreateDate())
-                .build();
+        GetUserResponseDto responseBody = new GetUserResponseDto(savedUser);
 
         return ResponseEntity.status(HttpStatus.OK).header(HttpHeaders.SET_COOKIE, cookie.toString()).body(responseBody);
 
@@ -76,7 +63,7 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public SignUpResponseDto signUp(SignUpRequestDto signUpRequestDto) {
         // 유저 확인
-        UserEntity user = userJpaRepository.findByEmail(signUpRequestDto.getEmail());
+        UserEntity user = authRepository.findUserByEmail(signUpRequestDto.getEmail());
         if(user != null) {
             if(passwordEncoder.matches(signUpRequestDto.getPassword(), user.getPassword())) {
                 throw new ConflictUserException();
@@ -84,12 +71,12 @@ public class AuthServiceImpl implements AuthService {
         }
 
         // 이메일 중복 확인
-        if (userJpaRepository.existsByEmail(signUpRequestDto.getEmail())) {
+        if (authRepository.userExistsByEmail(signUpRequestDto.getEmail())) {
             throw new ConflictEmailException();
         }
 
         // 닉네임 중복 확인
-        if (userJpaRepository.existsByNickname(signUpRequestDto.getNickname())) {
+        if(authRepository.userExistsByNickname(signUpRequestDto.getNickname())) {
             throw new ConflictNicknameException();
         }
 
@@ -111,85 +98,9 @@ public class AuthServiceImpl implements AuthService {
                 .build();
 
         // db 저장
-        userJpaRepository.save(newUser);
+        authRepository.saveUser(newUser);
 
         // 회원가입 성공
         return new SignUpResponseDto("SU", "회원가입 성공");
     }
-
-    // firebase 를 이용한 google 인증 후 google 인증 정보로 jwt 생성하여 헤더에 넣음
-    @Override
-    public ResponseEntity<GetUserResponseDto> authWithGoogle(AuthWithGoogleRequestDto requestDto) {
-        String idToken = requestDto.getIdToken();
-        boolean isSignup = requestDto.isSignUp();
-
-        try {
-            // firebase id 토큰 검증
-            FirebaseToken decodedToken = firebaseAuth.verifyIdToken(idToken);
-            String email = decodedToken.getEmail();
-            // 비밀번호로 사용
-            String uid = decodedToken.getUid();
-            // 닉네임으로 사용
-            String nickname = decodedToken.getName();
-
-            UserEntity alreadySignedUser = userJpaRepository.findByEmail(email);
-            UserEntity savedUser;
-
-            // 가입하는 경우 해당 이메일로 가입된 계정 있는지 검증
-            if(isSignup) {
-                if(alreadySignedUser != null) {
-                    throw new ConflictUserException();
-                }
-                // 유저 생성
-                UserEntity user = UserEntity.builder()
-                        .email(email)
-                        .password(uid)
-                        .nickname(nickname)
-                        .role(RoleName.ROLE_USER)
-                        .build();
-                // 구글 인증 표시
-                user.googleAuth();
-
-                savedUser = userJpaRepository.save(user);
-            } else {
-                // 로그인 시 회원정보가 DB 에 없으면 예외 발생
-                if(alreadySignedUser == null) {
-                    throw new UserNotFoundException();
-                }
-                savedUser = alreadySignedUser;
-            }
-
-            // jwt 쿠키 반환
-            ResponseCookie cookie = createCookie(savedUser, "/", true, "None", 60 * 60);
-
-            GetUserResponseDto responseBody = GetUserResponseDto.builder()
-                    .id(savedUser.getId())
-                    .email(savedUser.getEmail())
-                    .nickname(savedUser.getNickname())
-                    .role(savedUser.getRole())
-                    .profileImg(savedUser.getProfileImg())
-                    .createDate(savedUser.getCreateDate())
-                    .build();
-
-            // 쿠키를 헤더에 담고, 유저 정보를 바디에 담아 반환
-            return ResponseEntity.status(HttpStatus.OK).header(HttpHeaders.SET_COOKIE, cookie.toString()).body(responseBody);
-
-        } catch (FirebaseAuthException e) { // 토큰 검증 실패
-            throw new UnauthorizedException("AU", "firebase 인증 실패");
-        }
-    }
-
-    // 쿠키 생성
-    private ResponseCookie createCookie(UserEntity user, String path, boolean secure, String sameSite, int maxAge) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-        String jwt = jwtUtils.generateToken(authenticationToken);
-
-        return ResponseCookie.from("jwt", jwt)
-                .path(path) // 쿠키 사용 가능 path
-                .secure(secure) // https 에서만 사용 가능
-                .sameSite(sameSite) // 크로스 도메인 허용(실제 사용시에는 strict 나 lax 로 변경 필요)
-                .maxAge(maxAge) // 유효기간
-                .build();
-    }
-
 }
